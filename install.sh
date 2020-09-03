efa857937f4a10605577c2999094a9eafbc3ef5f
George Thayamkery <george.thayamkery@gmail.com>
2020-09-02 19:21:58 -0500

    updates

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# copy zshrc
+cp .zshrc ~/.zshrc
+
+# change shell to zsh
+chsh -s /bin/zsh
